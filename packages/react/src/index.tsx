b00a96ba8d805224b9e9973a072b1e5a4edeeed7
juniorcintra <junior.cintra.developer@gmail.com>
2025-05-08 15:28:16 -0300

    feat: :sparkles: Design System em React - Story: Button
    Design System em React - Story: Button

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,10 +1,31 @@
 import { styled } from "./styles";
+import { ComponentProps } from "react";
 
-const Button = styled("button", {
+export const Button = styled("button", {
   fontFamily: "$default",
-  backgroundColor: "$ignite500",
+  backgroundColor: "$ignite300",
+  borderRadius: "$sm",
+  padding: "$2 $4",
+  border: 0,
+  fontWeight: "$bold",
+  color: "$white",
+
+  variants: {
+    size: {
+      sm: {
+        fontSize: "$xs",
+        padding: "$2 $4",
+      },
+      lg: {
+        fontSize: "$md",
+        padding: "$3 $6",
+      },
+    },
+  },
+
+  defaultVariants: {
+    size: "sm",
+  },
 });
 
-export function App() {
-  return <Button>Hello World</Button>;
-}
+export type ButtonProps = ComponentProps<typeof Button>;
